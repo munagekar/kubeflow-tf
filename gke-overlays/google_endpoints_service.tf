ab03ae7ee166c38c2a0c8741cdc821645aad22de
Abhishek Munagekar <avm.abhishek@gmail.com>
2021-02-16 13:39:21 +0900

    Add CloudEndpoint

---
+++ gke-overlays/google_endpoints_service.tf
@@ -0,0 +1,9 @@
+resource "google_endpoints_service" "openapi_service" {
+  service_name = "cloud"
+  openapi_config = format(
+    file("openapi_spec.yml"),
+    var.cloud_endpoint,
+    var.cloud_endpoint,
+    google_compute_address.ip_address.address
+  )
+}
--- gke-overlays/variables.tf
+++ gke-overlays/variables.tf
@@ -1,9 +1,13 @@
-variable kf_name {
+variable "kf_name" {
   description = "Name of Kubeflow Cluster"
   type        = string
 }
 
-variable gcp_region {
+variable "gcp_region" {
   description = "Region for Kubeflow Cluster"
   type        = string
+}
+
+variable "cloud_endpoint" {
+  description = "Cloud Endpoint. Eg: api-name.endpoints.project-id.cloud.goog"
 }
