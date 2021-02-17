a10ecb430253778ff9971f6408e4a20bc2121f21
Abhishek Munagekar <avm.abhishek@gmail.com>
2021-02-17 15:04:22 +0900

    Fix Path

--- gke-overlays/google_endpoints_service.tf
+++ gke-overlays/google_endpoints_service.tf
@@ -1,7 +1,7 @@
 resource "google_endpoints_service" "openapi_service" {
   service_name = "cloud"
   openapi_config = format(
-    file("openapi_spec.yml"),
+    file("${path.module}/openapi_spec.yml"),
     var.cloud_endpoint,
     var.cloud_endpoint,
     google_compute_address.ip_address.address
