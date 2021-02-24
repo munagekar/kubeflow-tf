8904e76879e16068f750cfa227c608db03d49658
Abhishek Munagekar <avm.abhishek@gmail.com>
2021-02-24 17:20:51 +0900

    Remove Not Required Variable

--- gke-overlays/variables.tf
+++ gke-overlays/variables.tf
@@ -3,11 +3,6 @@ variable "kf_name" {
   type        = string
 }
 
-variable "gcp_region" {
-  description = "Region for Kubeflow Cluster"
-  type        = string
-}
-
 variable "cloud_endpoint" {
   description = "Cloud Endpoint. Eg: api-name.endpoints.project-id.cloud.goog"
 }
